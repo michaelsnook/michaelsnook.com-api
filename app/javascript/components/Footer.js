960827d5a979b39bda519ed5a769da8bfa61e6c2
Michael Snook <michael.snook@gmail.com>
2020-04-05 18:17:32 +0530

    Format code

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -7,7 +7,7 @@ function Footer(props) {
         <div className="col col-md-10 col-lg-8 offset-md-1 offset-lg-2 border-top ">
           <nav className="nav d-flex justify-content-around py-4">
             <a className="nav-link" href="/">home</a>
-            <a className="nav-link" href="https://twitter.com/MichaelSnook">twitter</a>
+            <a className="nav-link" href="https://twitter.com/michaelsnook">twitter</a>
             <a className="nav-link" href="https://facebook.com/michael.snook">facebook</a>
             <a className="nav-link" href="https://github.com/michaelsnook">github</a>
             <a className="nav-link disabled">CC0 1.0</a>
