1ba25380a04aceaf045481895c4a1ecbc5c7cdcc
Michael Snook <michael.snook@gmail.com>
2020-04-06 00:52:23 +0530

    Add fancy datestamp formatting

--- app/javascript/components/PostBody.js
+++ app/javascript/components/PostBody.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import renderMarkdown from './renderMarkdown';
 
+function dateStamp(text) {
+  const out = new Date(text);
+  try {
+    return out.toLocaleString('en-IN',
+      { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+    );
+  }
+  catch(e) {
+    return out.toDateString();
+  }
+}
+
 function PostBody(props) {
   return (
     <div className="row py-4 px-3">
       <div className="col-sm-12 col-lg-8 col-md-10 py-sm-3">
         <p className="text-muted"><em>
-          {props.created_at}
+          Posted on {dateStamp(props.created_at)}
         </em></p>
         <div
           dangerouslySetInnerHTML={{
