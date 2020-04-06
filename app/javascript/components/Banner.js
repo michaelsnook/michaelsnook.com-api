185925e3fc145c7cefbf65add8f8fe43956fcd9d
Michael Snook <michael.snook@gmail.com>
2020-04-06 12:38:03 +0530

    Handle blank image in banner

--- app/javascript/components/Banner.js
+++ app/javascript/components/Banner.js
@@ -3,11 +3,11 @@ import React from 'react';
 function Banner(props) {
   return (
     <div className="hero position-relative d-flex align-items-center justify-content-center w-100">
-      <img
+      {props.image && <img
         src={props.image}
         alt={`${props.title} image`}
         className="img-fluid position-absolute"
-      />
+      />}
       <div className="overlay bg-dark position-absolute" />
       <div className="position-relative text-white text-center">
         <h1 className="display-4 text-white">
