1d486c1605bc9b490e23c7d3f90fcd5c94354174
Michael Snook <me@michaelsnook.com>
2021-02-16 23:27:10 +0530

    Initial commit of working(?) Tailwind setup

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './app/**/*.html.erb',
+    './app/javascript/**/*.js',
+  ],
+  darkMode: media, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
