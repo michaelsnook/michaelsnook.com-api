3ef404b89782ea242d6670fa8c9c7fec08c43dc5
Michael Snook <me@michaelsnook.com>
2021-07-10 16:13:55 +0530

    Add component and route to list draft posts

---
+++ app/javascript/components/ListDraftPosts.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class ListDraftPosts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      user: null
+    };
+  }
+
+  componentDidMount() {
+    const url = '/api/v1/posts/drafts';
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then(response => this.setState({ posts: response }))
+  }
+
+  render() {
+    const { posts } = this.state;
+    const allPosts = posts.map((post, index) => (
+      <div key={index} className="col-md-6 col-lg-4 pb-4">
+        <div className="card h-100">
+          {post.image && <img
+            src={post.image}
+            className="card-img-top"
+            alt={`${post.title} image`}
+          />}
+          <div className="card-body d-flex align-items-start flex-column">
+            <h4 className="card-title">{post.title}</h4>
+            {!post.image && <p className="card-text">{post.excerpt}</p>}
+            <Link to={`/posts/${post.id}/update`} className="mt-auto btn btn-sm btn-outline-secondary">
+              Continue Drafting
+            </Link>
+          </div>
+        </div>
+      </div>
+    ));
+    const noPosts = (
+      <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
+        <h4>
+          No draft posts yet. Why not <Link to="/posts/new">create one</Link>
+        </h4>
+      </div>
+    );
+
+    return (
+      <>
+        <section className="hero position-relative d-flex align-items-center justify-content-center w-100">
+          <img className="Cover-image bottom" src="/como.jpg" alt="a picture of mountains" />
+          <div className="overlay bg-dark position-absolute"></div>
+          <div className="position-relative text-white container">
+            <div className="row">
+              <div className="col-12 col-md-8">
+                <h1 className="display-4 text-white">Michael Snook&rsquo;s site</h1>
+                <p className="text-white lead">A little weblog and project pad</p>
+              </div>
+              <div className="col-md-4 col-12">
+                <img id="my-face" src="/my-circle-drawing.png" alt="A comic sketch of Michael Snook" />
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className="py-5">
+          <main className="container">
+            <h2>Your Unfinished Drafts</h2>
+            { this.state.user && loginButton() }
+            <div className="row align-content-stretch d-flex flex-wrap">
+
+              {posts.length > 0 ? allPosts : noPosts}
+
+            </div>
+            <Link to="/" className="btn btn-link">
+              Home
+            </Link>
+          </main>
+        </div>
+      </>
+    );
+  }
+}
+export default ListDraftPosts;
--- app/javascript/routes/Index.js
+++ app/javascript/routes/Index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Posts from '../components/Posts';
+import ListDraftPosts from '../components/ListDraftPosts';
 import Post from '../components/Post';
 import NewPost from '../components/NewPost';
 import UpdatePost from '../components/UpdatePost';
@@ -12,6 +13,7 @@ export default (
       <Route path='/' exact component={Posts} />
       <Route path='/brand' exact component={Brand} />
       <Route path='/posts' exact component={Posts} />
+      <Route path='/posts/drafts' exact component={ListDraftPosts} />
       <Route path='/posts/new' exact component={NewPost} />
       <Route path='/posts/:id(\d+)' exact component={Post} />
       <Route path='/posts/:id(\d+)/update' exact component={UpdatePost} />
