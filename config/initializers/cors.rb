075488a552dbec4141ad7d9d642e3930993abe8d
Michael Snook <me@michaelsnook.com>
2021-07-17 17:42:50 +0530

    Accept requests from new client app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins 'localhost:3001', 'next.michaelsnook.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options, :head],
